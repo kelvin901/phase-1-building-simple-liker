802e3d7b6d698e23b7a94ad69ad9e7c7cb9a4354
kelvin901 <wawerukelvin2019@gmail.com>
2023-04-01 15:01:42 +0300

    Completed Assignment

--- main.js
+++ main.js
@@ -1,11 +1,46 @@
+
+
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const heart = document.querySelectorAll('.like-glyph');
+const modal = document.getElementById('modal');
+const modalMessage = document.getElementById('modal-message');
 
+for (let i = 0; i <heart.length; i++) {
+  heart[i].addEventListener('click', () => {
+    mimicServerCall()
+      .then(() => {
+        if (heart[i].classList.contains('activated-heart') === false) {
+          heart[i].classList.add('activated-heart');
+          heart[i].textContent = FULL_HEART;
+          // alert("Yes... Feed our algorithm! *evil laugh*");
+        } else if (heart[i].classList.contains('activated-heart') === true) {
+          heart[i].classList.remove('activated-heart');
+          heart[i].textContent = EMPTY_HEART;
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        showModal(error);
+      })
+  })
+}
 
+const showModal = (error) => {
+  modal.style.visibility = "visible";
+  modalMessage.textContent = error;
+  setTimeout(hideModal, 3000);
+}
+const hideModal = () => modal.style.visibility = "hidden";
+
+const fillHeart = (e) => {
+  console.log("filling heart");
+  heart.innerHTML = FULL_HEART;
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
